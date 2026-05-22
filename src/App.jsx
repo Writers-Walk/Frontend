8899a5accc1723f21e69f1c94e1b88fe5fc8bdff
jun9771 <jun97718@gmail.com>
2026-05-22 16:50:27 +0900

    confilict-merge

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,7 @@ import Layout from './Layout/components/Layout';
 
 import MainPage from './main/components/mainpage';
 import CoverGeneratePage from './CoverGeneratePage/CoverGeneratePage';
-<<<<<<< HEAD
 import BookDetailPage from './BookDetailPage/Hansu';
-
-export function App() {
-  const router = createBrowserRouter([
-    { path: "/", element: <MainPage /> },
-    { path: "/cover-generate/:id", element: <CoverGeneratePage /> },
-    { path: "/book/:id", element: <BookDetailPage /> }
-  ]);
-  
-  return <RouterProvider router={router} />;
-}
-
-export default App;
-=======
 import BookCreate from './pages/BookCreate';
 
 
@@ -31,6 +17,7 @@ export function App() {
         { path: "/", element: <MainPage /> },
         { path: "/cover-generate/:id", element: <CoverGeneratePage /> },
         { path: "/book-create", element: <BookCreate /> },
+         { path: "/book/:id", element: <BookDetailPage /> },
       ],
     },
   ]);
@@ -39,4 +26,4 @@ export function App() {
 }
 
 export default App;
->>>>>>> e8e5212b57de5a0ea68ca3358c6151900eb53f7f
+
