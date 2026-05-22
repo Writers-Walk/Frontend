03ee635adadb58789a54be1db781d789df3a7830
badapyobum <gkstnghkdlxld@gmail.com>
2026-05-22 17:11:24 +0900

    feat: merge충돌 해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import './App.css';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
 import Layout from './Layout/components/Layout';
-
 import MainPage from './main/components/mainpage';
 import CoverGeneratePage from './CoverGeneratePage/CoverGeneratePage';
-<<<<<<< HEAD
 import BookDetailPage from './BookDetailPage/Hansu';
 
 export function App() {
@@ -19,24 +16,4 @@ export function App() {
 }
 
 export default App;
-=======
-import BookCreate from './pages/BookCreate';
 
-
-export function App() {
-  const router = createBrowserRouter([
-    {
-      element: <Layout />,
-      children: [
-        { path: "/", element: <MainPage /> },
-        { path: "/cover-generate/:id", element: <CoverGeneratePage /> },
-        { path: "/book-create", element: <BookCreate /> },
-      ],
-    },
-  ]);
-
-  return <RouterProvider router={router} />;
-}
-
-export default App;
->>>>>>> e8e5212b57de5a0ea68ca3358c6151900eb53f7f
